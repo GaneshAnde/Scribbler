a78d2d24195654b593a9d54e30620ec71ca77b37
GaneshAnde <abnpganesh@gmail.com>
2023-11-26 23:57:25 +0530

    yes no confirmation added for deleting a post

--- Course+6+Project+Stub+-+Scribbler/script/postslist.js
+++ Course+6+Project+Stub+-+Scribbler/script/postslist.js
@@ -1,10 +1,42 @@
+var deleteBtn;
 function openPostPage(){
     console.log("siiga")
     window.location.href = '../html/post.html';
 }
 
 function onClickTrash(deleteButton) {
-    var card = deleteButton.closest('.card');
+    deleteBtn = deleteButton;
+    displayYesNoModal();
+    // document.body.style.backgroundColor = 'rgb(165,165,165)'
+    // document.getElementById('trashModal').style.display = 'block';
+   
+    
+}
+
+function dismissYesNoModel(){
+    var overlay = document.getElementById('overlay');
+    var modal = document.getElementById('modal');
+    // Toggle the active class on both overlay and modal
+            overlay.classList.toggle('active');
+            modal.classList.toggle('active');            
+}
+
+function displayYesNoModal(){
+    var overlay = document.getElementById('overlay');
+    var modal = document.getElementById('modal');
+    // Toggle the active class on both overlay and modal
+            overlay.classList.toggle('active');
+            modal.classList.toggle('active');
+}
+
+function onClickYes(){
+    
+    var card = deleteBtn.closest('.card');
     var cardContainer = document.getElementById("cardContainer");
     cardContainer.removeChild(card);
+    dismissYesNoModel();
+}
+
+function onClickNo(){
+    dismissYesNoModel();
 }
